1fbbcc5aa8e341409ba3c98373d201d2b11b2c49
Edward Loveall <edward@edwardloveall.com>
2017-06-08 19:57:26 -0400

    Remove mountain class :(

--- TestSaver/MainView.swift
+++ TestSaver/MainView.swift
@@ -1,7 +1,7 @@
 import ScreenSaver
 
 class MainView: ScreenSaverView {
-  var mountains = Mountains(bounds: CGRect.zero)
+//  var mountains = Mountains(bounds: CGRect.zero)
   var offset: CGFloat = 0;
 
   override init?(frame: NSRect, isPreview: Bool) {
@@ -16,19 +16,19 @@ class MainView: ScreenSaverView {
 
   func setup() {
     animationTimeInterval = 1 / 24
-    mountains.bounds = bounds
+//    mountains.bounds = bounds
   }
 
   override func draw(_ rect: NSRect) {
     super.draw(rect)
     NSColor(calibratedRed:0.96, green:0.95, blue:0.93, alpha:1).setFill()
     NSRectFill(bounds)
-    mountains.draw()
+//    mountains.draw()
   }
 
   override func animateOneFrame() {
     setNeedsDisplay(bounds)
     offset -= 1
-    mountains.draw()
+//    mountains.draw()
   }
 }
